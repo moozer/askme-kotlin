c7e42ae13817ac9e747b4c8501c98a6fa9493e88
moz <moz@local>
2018-03-31 15:23:27 +0200

    first commit

---
+++ app/src/main/java/world/my/moz/askme/Question_1.kt
@@ -0,0 +1,12 @@
+package world.my.moz.askme
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Question_1 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_question_1)
+    }
+}
